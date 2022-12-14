127ec8de4220ab28ed1f872f77f9e641edbc13a1
Michael Maeng <mmaeng@amazon.com>
2022-12-14 10:21:57 -0500

    Fix integ tests for Dictionary:Dictionary

--- Fastly-Dictionary-Dictionary/setup.sh
+++ Fastly-Dictionary-Dictionary/setup.sh
@@ -16,6 +16,6 @@ mkdir -p inputs
 cat example-inputs/inputs_1_create.json | sed "s/<FASTLY_SERVICE_ID>/${FASTLY_SERVICE_ID}/g" | sed "s/<FASTLY_VERSION_ID>/${FASTLY_VERSION_ID}/g" > inputs/inputs_1_create.json
 cat example-inputs/inputs_1_update.json | sed "s/<FASTLY_SERVICE_ID>/${FASTLY_SERVICE_ID}/g" | sed "s/<FASTLY_VERSION_ID>/${FASTLY_VERSION_ID}/g" > inputs/inputs_1_update.json
 cp example-inputs/inputs_1_invalid.json inputs/
-cat test/integ.yml | sed "s/<FASTLY_SERVICE_ID>/${FASTLY_SERVICE_ID}/g" | sed "s/<FASTLY_VERSION_ID>/${FASTLY_VERSION_ID}/g" | sed "s/<TEST_DICTIONARY>/$(uuidgen)/" > test/integ-unique.yml
+cat test/integ.yml | sed "s/<FASTLY_SERVICE_ID>/${FASTLY_SERVICE_ID}/g" | sed "s/<FASTLY_VERSION_ID>/${FASTLY_VERSION_ID}/g" > test/integ-unique.yml
 
 python3 ../get_type_configuration.py
