b9c7b105c9f714b537f934b735ad2ba93defed48
Michael Maeng <mmaeng@amazon.com>
2022-12-16 15:02:03 -0500

    move letter to setup.sh

--- Fastly-Dictionary-DictionaryItem/setup.sh
+++ Fastly-Dictionary-DictionaryItem/setup.sh
@@ -13,7 +13,7 @@
 
 mkdir -p inputs
 
-RANDOM_DICTIONARY=$(uuidgen | sed 's/[-]//g')
+RANDOM_DICTIONARY=a$(uuidgen | sed 's/[-]//g')
 
 cat example-inputs/inputs_1_create.json | sed "s/<FASTLY_SERVICE_ID>/${FASTLY_SERVICE_ID}/g" | sed "s/<FASTLY_DICTIONARY_ID>/{{DictionaryIdExport}}/g" > inputs/inputs_1_create.json
 cat example-inputs/inputs_1_update.json | sed "s/<FASTLY_SERVICE_ID>/${FASTLY_SERVICE_ID}/g" | sed "s/<FASTLY_DICTIONARY_ID>/{{DictionaryIdExport}}/g" > inputs/inputs_1_update.json
